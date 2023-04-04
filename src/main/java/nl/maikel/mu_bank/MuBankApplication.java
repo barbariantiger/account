bc71595c306ab9579dfa40dc5bac4a4c9ba4a3fd
Maikel Maas <barbariantiger@yahoo.com.br>
2023-04-04 23:06:09 +0200

    User Story 31 - As a customer I want to be able to deposit money into my account
       - Set basic structure of the project;

---
+++ src/main/java/nl/maikel/mu_bank/MuBankApplication.java
@@ -0,0 +1,13 @@
+package nl.maikel.mu_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuBankApplication.class, args);
+    }
+
+}

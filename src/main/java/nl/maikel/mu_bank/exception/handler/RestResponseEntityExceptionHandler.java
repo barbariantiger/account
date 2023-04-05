754a301b1af305adc51d0fa7ee5dc497ca340d23
Maikel Maas <barbariantiger@yahoo.com.br>
2023-04-06 00:42:20 +0200

    User Story 31 - As a customer I want to be able to deposit money into my account
       - Added integration tests;
       - Improved validation;

--- src/main/java/nl/maikel/mu_bank/exception/handler/RestResponseEntityExceptionHandler.java
+++ src/main/java/nl/maikel/mu_bank/exception/handler/RestResponseEntityExceptionHandler.java
@@ -7,14 +7,34 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex, final HttpHeaders headers, final HttpStatusCode status, final WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        var error = new Error(400, "Bad Request", String.join(", ", errors.values()));
+        return handleExceptionInternal(ex, error, headers, status, request);
+    }
+
     @ExceptionHandler({ ConstraintViolationException.class })
     public ResponseEntity<Object> handleBadRequest(final ConstraintViolationException ex, final WebRequest request) {
         var error = new Error(400, "Bad Request", ex.getMessage());
